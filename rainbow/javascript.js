d4618bfcfccbe033a8e8185b6f9f06036cc70cb3
Patience Mothoa <91294052+PatienceMothoa@users.noreply.github.com>
2021-10-30 12:45:44 +0200

    Add files via upload

---
+++ rainbow/javascript.js
@@ -0,0 +1,34 @@
+function Red(){
+    document.getElementById("red").style.backgroundColor="red";
+    document.getElementById("red").innerHTML="Red"
+}
+
+function Orange(){
+    document.getElementById("orange").style.backgroundColor="orange";
+    document.getElementById("orange").innerHTML="Orange"
+}
+
+function Yellow(){
+    document.getElementById("yellow").style.backgroundColor="yellow";
+    document.getElementById("yellow").innerHTML="Yellow"
+}
+
+function Green(){
+    document.getElementById("green").style.backgroundColor="green";
+    document.getElementById("green").innerHTML="Red"
+}
+
+function Blue(){
+    document.getElementById("blue").style.backgroundColor="blue";
+    document.getElementById("blue").innerHTML="Blue"
+}
+
+function Indigo(){
+    document.getElementById("indigo").style.backgroundColor="indigo";
+    document.getElementById("indigo").innerHTML="Indigo"
+}
+
+function Purple(){
+    document.getElementById("purple").style.backgroundColor="purple";
+    document.getElementById("purple").innerHTML="Purple"
+}
